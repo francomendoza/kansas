9e62c5f195e81004b26404259cbb0102ac30e850
francomendoza <frm210@gmail.com>
2015-07-12 12:48:55 -0400

    add page nav

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,5 +1,13 @@
 class LibrariesController < ApplicationController
   def index
-    @libraries = Library.all
+    # first = Library.first
+    # array = []
+    # Library.all.each do |lib|
+    #   if array.length < 8 
+    #   array << Math.sqrt((lib.latitude-first.latitude)**2+(lib.longitude-first.longitude)**2)
+    # else
+      
+    # end
+    @libraries = Library.limit(8)
   end
 end
--- app/models/library.rb
+++ app/models/library.rb
@@ -13,7 +13,6 @@ class Library < ActiveRecord::Base
       # data should likely be cleaned up a bit prior to inputting to database
       unless index == 0 #excludes column headers
         self.create(name: row.children[1].text, street_address: row.children[2].text, city: row.children[0].text, zipcode: row.children[3].text, number: row.children[4].text)
-
       end
     end
   end
